deff1bf8ecb39300f237feb5e3e61bdeeba18336
MasashiHamano <marcyyy0612@gmail.com>
2022-06-07 18:47:26 +0900

    Add 002

---
+++ contents/002/test/test.sh
@@ -0,0 +1,5 @@
+java -version
+
+# openjdk version "1.8.0_322"
+# OpenJDK Runtime Environment Corretto-8.322.06.1 (build 1.8.0_322-b06)
+# OpenJDK 64-Bit Server VM Corretto-8.322.06.1 (build 25.322-b06, mixed mode)
