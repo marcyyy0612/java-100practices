1e33a4e9c16c1847018c9ef505fd9361e7dfcd5f
MasashiHamano <marcyyy0612@gmail.com>
2022-06-16 15:55:47 +0900

    Add 007

---
+++ contents/007/java/Answer.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Answer {
+  public static void main(String arg[]) {
+    System.out.println("StringUtils.isEmpty(null): " + StringUtils.isEmpty(null));
+  }
+}
---
+++ contents/007/test/test.sh
@@ -0,0 +1,2 @@
+javac -cp ../java/lib/commons-lang3-3.12.0.jar ../java/Answer.java -d ../java/classes
+java -cp ../java/lib/commons-lang3-3.12.0.jar:../java/classes Answer
